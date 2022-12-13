2e820f95dc9c11b299d767ff4968a914ba934c79
Vis <vt201916384@gmail.com>
2022-12-12 23:08:45 -0500

    finished options (testing needed)

---
+++ Options/options.js
@@ -0,0 +1,14 @@
+const onSubmit = event => {
+    event.preventDefault();
+    chrome.storage.local.get({blocked: []}, result => {
+        const blocked = result.blocked;
+        const text = event.elements.links.split("\n");
+        for(let i = 0; i < text.length; i++) blocked.push("text");
+        chrome.storage.local.set({blocked: blocked}, () => {
+            chrome.storage.local.get('blocked', (result) => {console.log(result.blocked)});
+        })
+    })
+}
+
+const form = document.getElementById("form");
+form.addEventListener("submit", onSubmit);
