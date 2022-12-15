e08e7009157600df7e746fcc73a6425eb0a7b4df
Vis <vt201916384@gmail.com>
2022-12-14 22:13:45 -0500

    fix blocked.html path

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 document.getElementById("button").addEventListener('click', () => {
-    chrome.tabs.create({url: "./blocked.htmlblocked.html"})
+    chrome.tabs.create({url: "./blocked.html"})
 })
 
 // add stats display maybe in separate function
