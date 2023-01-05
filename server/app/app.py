83b2c4de7458b58988149c6ec456ee1fd0a20464
VisH317 <vt201916384@gmail.com>
2023-01-04 20:54:03 -0500

    sam testing again

--- server/app/app.py
+++ server/app/app.py
@@ -1,4 +1,5 @@
 import torch
+import pandas as pd
 import json
 import csv
 import torch.nn as nn
@@ -7,22 +8,22 @@ import torch.nn.functional as F
 from transformers import BertTokenizer, BertForSequenceClassification
 
 # access model params from bucket
-import boto3
-bucket_name = "ml-bucket"
-model_parameters_file_name = "model.pt"
-categories_file_name = "categories.csv"
-s3_resource = boto3.resource('s3')
-s3_resource.Object(bucket_name, model_parameters_file_name).download_file(f'/opt/ml/model')
-s3_resource.Object(bucket_name, categories_file_name).download_file(f'/opt/ml/categories')
+# import boto3
+# bucket_name = "ml-bucket"
+# model_parameters_file_name = "model.pt"
+# categories_file_name = "categories.csv"
+# s3_resource = boto3.resource('s3')
+# s3_resource.Object(bucket_name, model_parameters_file_name).download_file(f'/opt/ml/model')
+# s3_resource.Object(bucket_name, categories_file_name).download_file(f'/opt/ml/categories')
 
 device = torch.device('cpu')
 
 class WebsiteClassifier(nn.Module):
-    def __init__(self, ds):
+    def __init__(self):
         super().__init__()
         # title and url feature extractor BERTs
-        self.title_model = BertForSequenceClassification.from_pretrained('prajjwal1/bert-tiny', num_labels=384).to(device, dtype=torch.float32)
-        self.url_model = BertForSequenceClassification.from_pretrained('prajjwal1/bert-tiny', num_labels=384).to(device, dtype=torch.float32)
+        self.title_model = BertForSequenceClassification.from_pretrained('prajjwal1/bert-tiny', num_labels=384).to(dtype=torch.float32)
+        self.url_model = BertForSequenceClassification.from_pretrained('prajjwal1/bert-tiny', num_labels=384).to(dtype=torch.float32)
                 
         # classifier network
         self.classifier = nn.Sequential(
@@ -34,27 +35,27 @@ class WebsiteClassifier(nn.Module):
             nn.LayerNorm(600),
             nn.Linear(600, 588),
             nn.Softmax()
-        ).to(device, dtype=torch.float32)
+        ).to(dtype=torch.float32)
                
         self.optim = torch.optim.AdamW(self.parameters(), 1e-3)
-        self.ds = ds
     
     def forward(self, title, url):
-        title_output = self.title_model(**title).logits.to(device, dtype=torch.float32).squeeze()
-        url_output = self.url_model(**url).logits.to(device, dtype=torch.float32).squeeze()
-        concated = torch.concat((title_output, url_output), dim=0).to(device, dtype=torch.float32)
-        output = self.classifier(concated).to(device, dtype=torch.float32)
+        print(title)
+        title_output = self.title_model(**title).logits.to(dtype=torch.float32).squeeze()
+        url_output = self.url_model(**url).logits.to(dtype=torch.float32).squeeze()
+        concated = torch.cat+((title_output, url_output), dim=0).to(dtype=torch.float32)
+        output = self.classifier(concated).to(dtype=torch.float32)
         return output
 
 
-model_file = '/opt/ml/model'
-tokenizer = BertTokenizer.from_pretrained('prajjwal/bert-tiny')
+model_file = './model/model.pt'
+tokenizer = BertTokenizer.from_pretrained('prajjwal1/bert-tiny')
 model = WebsiteClassifier()
-model.load_state_dict(torch.load(model_file))
+model.load_state_dict(torch.load(model_file, map_location=torch.device('cpu')))
 
-cats_file = open("/opt/ml/categories.csv", "r")
-categories = list(csv.reader(cats_file, deliminter=","))
-categories = [cat[1] for cat in categories]
+cats_file = pd.read_csv("./model/categories.csv")
+categories = list(csv.reader(cats_file, delimiter=","))
+categories = list(cats_file["0"])
 
 def get_top_categories(probabilities):
     values,top3 = torch.topk(probabilities, 3)
@@ -63,20 +64,12 @@ def get_top_categories(probabilities):
         top_cats.append(categories[idx])
     return top_cats
 
-# things to do: get the categories list and match them to probabilities, send the top 3 as a request
-# make a local API to test
-# setup the queue service for the model
-# research lambda authorizer
-# research storing model params in S3 - done
-# deploy and test
-# styles, stats, etc.
-
 def website_inference(event, context):
-    title = event['body']['title']
-    url = event['body']['url']
+    title = event['body']['Title']
+    url = event['body']['URL']
 
-    title_embed = tokenizer(title)
-    url_embed = tokenizer(url)
+    title_embed = tokenizer(title, return_tensors='pt')
+    url_embed = tokenizer(url, return_tensors='pt')
 
     probabilities = model.forward(title_embed, url_embed)
     label = get_top_categories(probabilities)
@@ -89,3 +82,11 @@ def website_inference(event, context):
             }
         )
     }
+
+# things to do: get the categories list and match them to probabilities, send the top 3 as a request
+# make a local API to test
+# setup the queue service for the model
+# research lambda authorizer
+# research storing model params in S3 - done
+# deploy and test
+# styles, stats, etc.
