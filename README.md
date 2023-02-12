d1c5cdbcf0ceb7ae2e2441f7b9769e199185c330
Vis <vt201916384@gmail.com>
2023-02-11 19:25:21 -0500

    popup & blocked css done

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ A good website blocker that fits my needs so I can be more productive
 #### To-dos
  * Update the machine learning model to be more efficient: possibly feature extraction instead of passing the whole thing (paths list, domain analysis, etc.)
  * Add styles
+ * Stats display
  * Create AWS Serverless instance
--- extension/background.js
+++ extension/background.js
@@ -49,35 +49,35 @@ chrome.tabs.onUpdated.addListener(async function(tabId, changeInfo, tab) {
         // call AI
         shouldBlock = false
         const isHTTP = tab.url.split(":")[0]=="http" || tab.url.split(":")[0]=="https"
-        if(isHTTP) {
-            console.log(tab.url)
-            console.log(tab.title)
-            const request = {
-                URL: tab.url,
-                Title: tab.title
-            }
-            const res = await fetch("http://127.0.0.1:3000/video", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(request)
-            })
-            const label_text = await res.text()
-            console.log(label_text)
-            const { predicted_label } = JSON.parse(label_text)
-            console.log("Got predicted labels!!")
-            console.log(predicted_label)
-            labels = []
-            for(let i=0;i<predicted_label.length;i++) {
-                if(labels!="NaN") labels.push(predicted_label[i])
-            }
-            for(let i=0;i<labels.length;i++) {
-                if(breakLabels.includes(labels[i])) shouldBlock = true
-            }
-        }
+        // if(isHTTP) {
+        //     console.log(tab.url)
+        //     console.log(tab.title)
+        //     const request = {
+        //         URL: tab.url,
+        //         Title: tab.title
+        //     }
+        //     const res = await fetch("http://127.0.0.1:3000/video", {
+        //         method: "POST",
+        //         headers: {
+        //             "Content-Type": "application/json"
+        //         },
+        //         body: JSON.stringify(request)
+        //     })
+        //     const label_text = await res.text()
+        //     console.log(label_text)
+        //     const { predicted_label } = JSON.parse(label_text)
+        //     console.log("Got predicted labels!!")
+        //     console.log(predicted_label)
+        //     labels = []
+        //     for(let i=0;i<predicted_label.length;i++) {
+        //         if(labels!="NaN") labels.push(predicted_label[i])
+        //     }
+        //     for(let i=0;i<labels.length;i++) {
+        //         if(breakLabels.includes(labels[i])) shouldBlock = true
+        //     }
+        // }
         domain = tab.url.split("/")[2].split(".").slice(1,3).join(".")
-        if(isHTTP && blocked.includes(domain)) {
+        if(/*isHTTP && */blocked.includes(domain)) {
             chrome.tabs.update(tab.id, {
                 url: "./blocked.html"
             })
